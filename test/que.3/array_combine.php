a5341bb7cf39bb4b9a2234fde5fa4f47eef9a0e7
kripalsinh1993 <110216713+kripalsinh1993@users.noreply.github.com>
2022-10-19 10:36:41 +0530

    Add files via upload

---
+++ test/que.3/array_combine.php
@@ -0,0 +1,12 @@
+<?php
+
+    //array combine is combine all the array
+     $no=array("11","22","33");
+     $a=array("tuv","wxyz","mno");
+     
+
+     $arr=array_combine($no,$a);
+
+     print_r($arr);
+
+?>
---
+++ test/que.3/array_merge.php
@@ -0,0 +1,14 @@
+<?php
+
+
+//array merge is return its key with value and merge array
+
+$no=array("11","22","33");
+$a=array("tuv","wxyz","mno");
+
+
+$arr=array_merge($no,$a);
+
+print_r($arr);
+
+?>
